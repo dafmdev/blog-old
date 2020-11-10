1bef6c0d1fee373faa2ff817a333b52cadf9c938
Daniel Franco <dfranco123@hotmail.com>
2020-11-10 11:53:47 -0500

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -209,7 +209,7 @@ class BlogPostTemplate extends React.Component {
               style={{
                 boxShadow: 'none',
                 textDecoration: 'none',
-                color: 'var(--pink)',
+                color: 'var(--blue)',
               }}
               to={'/'}
             >
