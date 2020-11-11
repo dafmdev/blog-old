2a823367d788520d488dcc74f39853170d1b9c42
fdf92441 <dfranco123@hotmail.com>
2020-11-11 13:44:53 -0500

    Correción de nombre

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,7 +13,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Dan Abramov`}
+          alt={`Daniel Franco`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
